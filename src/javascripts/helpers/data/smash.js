1db518668a1a15ce37071f127f8b5effa1ce5d68
John Thielman <john.thielman@gmail.com>
2019-11-02 13:11:02 -0500

    getting all snacks by uid

--- src/javascripts/helpers/data/smash.js
+++ src/javascripts/helpers/data/smash.js
@@ -1,6 +1,7 @@
 import machineData from './machineData';
 import positionData from './positionData';
 import snackPositionData from './snackPositionData';
+import snackData from './snackData';
 
 const getCompleteMachine = () => new Promise((resolve, reject) => {
   // 1. getMachines -- returns first machine (hard coding) -- DONE
@@ -13,7 +14,12 @@ const getCompleteMachine = () => new Promise((resolve, reject) => {
     .then((singleMachine) => positionData.getAllPositionsByMachineId(singleMachine.id))
     .then((positions) => {
       snackPositionData.getAllSnackPositionsByMachineId(positions[0].machineId)
-        .then((snackPositions) => resolve(snackPositions));
+        .then((snackPositions) => {
+          snackData.getSnacksByUid(positions[0].uid).then((snacks) => {
+            console.log('snackPositions', snackPositions);
+            resolve(snacks);
+          });
+        });
     })
     .catch((error) => reject(error));
 });
