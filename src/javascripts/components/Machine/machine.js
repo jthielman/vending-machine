68dd4a000cdbe6591fa745aacf057837420f6bf0
John Thielman <john.thielman@gmail.com>
2019-11-05 20:34:34 -0600

    buy works

--- src/javascripts/components/Machine/machine.js
+++ src/javascripts/components/Machine/machine.js
@@ -1,8 +1,19 @@
+import $ from 'jquery';
 import smash from '../../helpers/data/smash';
 import snacks from '../Snacks/snacks';
 
 import './machine.scss';
 import utilities from '../../helpers/utilities';
+import snackData from '../../helpers/data/snackData';
+
+const buySnack = (e) => {
+  e.stopImmediatePropagation();
+  const snackId = e.target.id.split('buy-')[1];
+  snackData.buySnack(snackId)
+    // eslint-disable-next-line no-use-before-define
+    .then(() => buildTheMachine())
+    .catch((error) => console.error(error));
+};
 
 const buildTheMachine = () => {
   smash.getCompleteMachine()
@@ -14,6 +25,7 @@ const buildTheMachine = () => {
       });
       domString += '</div>';
       utilities.printToDom('machine', domString);
+      $('#machine').on('click', '.buy-snack', buySnack);
       // Build a dom string
       // h2 that says VENDING MACHINE
       // div with an id = snack-section, class=d-flex flex-wrap
